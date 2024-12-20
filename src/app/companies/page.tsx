120c6ad8c776ac4e889b0934600c4a932857c551
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-12-20 08:59:51 +0000

    basic structure for routing added

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/app/components/Header';
+
+const Page = () => {
+  return (
+    <div>
+      <p className="text-xl">Settings Page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p className="text-xl">Settings Page</p>
+    </div>
+  );
+};
+
+export default Page;
