90e49913b55f38a9eaa284c7ee1bd1e1cb4b06bb
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2025-01-17 11:28:12 +0000

    company promotion added

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from 'react';
 import CompanyTable from '../../components/CompanyTable';
-import CompanyRow from '../../components/CompanyRow';
-import { Status } from '@/app/components/StatusLabel';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { getCompanies } from '@/lib/api';
 import getQueryClient from '@/lib/utils/getQueryClient';
@@ -23,44 +21,6 @@ export default async function Page() {
 
   return (
     <div>
-      {/* <CompanyTable>
-        <CompanyRow
-          id={1}
-          category="Products"
-          company="Costco"
-          status={Status.Pending}
-          promotion={true}
-          country="USA"
-          joinedDate="02.10.2023"
-        />
-        <CompanyRow
-          id={2}
-          category="Products"
-          company="Aldi"
-          status={Status.Active}
-          promotion={true}
-          country="South America"
-          joinedDate="07.08.2022"
-        />
-        <CompanyRow
-          id={3}
-          category="Products"
-          company="ATБ"
-          status={Status.Suspended}
-          promotion={false}
-          country="Ukraine"
-          joinedDate="10.30.2022"
-        />
-        <CompanyRow
-          id={4}
-          category="Products"
-          company="Apple"
-          status={Status.NotActive}
-          promotion={false}
-          country="North America"
-          joinedDate="05.04.2021"
-        />
-      </CompanyTable> */}
       <HydrationBoundary state={dehydratedState}>
         <CompanyTable />
       </HydrationBoundary>
